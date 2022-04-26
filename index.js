ec28ab243f617e896b9e55d9f01e51864f28fa74
Amplify Rebel <erj2204@gmail.com>
2022-04-25 23:08:36 -0500

    added the api functions

---
+++ index.js
@@ -0,0 +1,29 @@
+const PORT = process.env.PORT || 3001;
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+const app = express();
+
+const allNotes = require('./db/notes.json');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './notetaker.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'));
+});
+
